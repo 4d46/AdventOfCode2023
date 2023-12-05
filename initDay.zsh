1c5a271c9a5b3390f962f3f7eda24b775926881e
4d46 <dev@4d46.uk>
2023-12-05 22:03:19 +0000

    Update init script to include file loading in template

--- initDay.zsh
+++ initDay.zsh
@@ -35,6 +35,17 @@ import \"fmt\"
 
 func main() {
     fmt.Printf(\"Advent of Code 2023 - Day %2d\\\n\", ${1})
+}
+
+// Load file contents into a string and return it
+func loadFileContents(filename string) string {
+        // Read contents of file into a string
+        fileBytes, err := os.ReadFile(filename) // just pass the file name
+        if err != nil {
+                panic(err)
+        }
+
+        return string(fileBytes) // convert content to a 'string'
 }" > main.go
 
 go build
