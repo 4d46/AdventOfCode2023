b12378cb36f5ae448f32a8c12f837cf2038fc783
4d46 <dev@4d46.uk>
2023-12-20 20:23:03 +0000

    Day 19

---
+++ day19/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+    fmt.Printf("Advent of Code 2023 - Day %2d\n", 19)
+}
+
+// Load file contents into a string and return it
+func loadFileContents(filename string) string {
+        // Read contents of file into a string
+        fileBytes, err := os.ReadFile(filename) // just pass the file name
+        if err != nil {
+                panic(err)
+        }
+
+        return string(fileBytes) // convert content to a 'string'
+}
